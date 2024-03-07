fc3dabcced1f509d34b241c0da666e390153e174
Jere-my9 <145876982+Jere-my9@users.noreply.github.com>
2024-03-07 18:23:00 -0500

    Update movimientos.js

--- movimientos.js
+++ movimientos.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Código JavaScript para manipular la animación de la rosa, si es necesario
+function bloomRose() {
+    const rose = document.getElementById('rose');
+    rose.style.opacity = 1;
+}
+
+function changeBackgroundColor() {
+    const body = document.querySelector('body');
+    const currentColor = body.style.backgroundColor;
+    body.style.backgroundColor = currentColor === 'rgb(240, 240, 240)' ? '#ffff99' : '#f0f0f0';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(bloomRose, 1000); // Espera 1 segundo antes de hacer florecer la rosa
 });
